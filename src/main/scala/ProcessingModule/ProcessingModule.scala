3c798e3add99472d1304ef80b2640b383bd7bae3
Allen Baker <althebaker43@gmail.com>
2020-01-24 20:01:15 -0800

    Refactored ProcessingModule to remove AdderModule references

--- src/main/scala/ProcessingModule/ProcessingModule.scala
+++ src/main/scala/ProcessingModule/ProcessingModule.scala
@@ -3,71 +3,32 @@ package ProcessingModule
 
 import chisel3._
 
-class DataIO[T <: Data, U <: Data](inValueType : T, outValueType : U) extends Bundle {
-  val in = Flipped(util.Decoupled(inValueType))
-  val out = outValueType
+abstract class Instructions {
+
+  def logic : Seq[InstructionLogic]
 }
 
-class AdderOutput(val dWidth : Int) extends Bundle {
-  val memReq = util.Valid(UInt(4.W))
-  val storeVal = util.Decoupled(UInt(dWidth.W))
-}
+abstract class InstructionLogic (val name : String, val dataInDepend : Boolean, val dataOutDepend : Boolean) {
 
-class InstrIO(iWidth : Int) extends Bundle {
-  val in = Flipped(util.Decoupled(UInt(iWidth.W)))
-  val pc = util.Valid(UInt(64.W))
-}
+  def decode( instr : UInt) : Bool
 
-object AdderModule {
-
-  val INSTR_NOP = 0
-  val INSTR_INCR_1 = 1
-  val INSTR_INCR_DATA = 2
-  val INSTR_STORE = 3
-  val INSTR_BGT = 4
-}
-
-object AdderInstruction {
-
-  val codeNOP :: codeIncr1 :: codeIncrData :: codeStore :: codeBGT :: Nil = util.Enum(5)
-
-  val codeWidth = codeNOP.getWidth
-
-  val regWidth = 1
-
-  val width = codeWidth + regWidth
-
-  def apply(codeLit : Int, regLit : Int) : AdderInstruction = new AdderInstruction(codeLit.U, regLit.U)
-
-  def nop : AdderInstruction = new AdderInstruction(codeNOP, 0.U)
-
-  def incr1(regLit : Int) : AdderInstruction = new AdderInstruction(codeIncr1, regLit.U)
-
-  def incrData(regLit : Int) : AdderInstruction = new AdderInstruction(codeIncrData, regLit.U)
-
-  def store(regLit : Int) : UInt = (new AdderInstruction(codeStore, regLit.U)).asUInt
-
-  def bgt(regLit : Int) : AdderInstruction = new AdderInstruction(codeBGT, regLit.U)
-
-  def createInt(codeVal : UInt, regVal : UInt) : BigInt = (new AdderInstruction).createInt(codeVal, regVal)
-}
-
-class AdderInstruction(val code : UInt = UInt(AdderInstruction.codeWidth.W), val reg : UInt = UInt(AdderInstruction.regWidth.W)) extends Bundle {
-  require(code.getWidth == AdderInstruction.codeWidth)
-  require(reg.getWidth == AdderInstruction.regWidth)
-
-  def createInt(codeVal : UInt, regVal : UInt) : BigInt = {
-    def catValWidths(valWidthA : Tuple2[BigInt, Int], valWidthB : Tuple2[BigInt, Int]) : Tuple2[BigInt, Int] = {
-      (((valWidthB._1 << valWidthA._2) | valWidthA._1), (valWidthA._2 + valWidthB._2))
-    }
-    ((((codeVal :: regVal :: Nil) map (_.litValue)) zip (getElements map (_.getWidth))) reduce catValWidths)._1
-  }
+  def execute( instr : UInt) : Unit
 }
 
 abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) extends Module {
+
   val io = IO(new Bundle {
-    val data = new DataIO(UInt(dWidth.W), new AdderOutput(dWidth))
-    val instr = new InstrIO(iWidth)
+    val instr = new Bundle {
+      val in = Flipped(util.Decoupled(UInt(iWidth.W)))
+      val pc = util.Valid(UInt(64.W))
+    }
+    val data = new Bundle {
+      val in = Flipped(util.Decoupled(UInt(dWidth.W)))
+      val out = new Bundle {
+        val memReq = util.Valid(UInt(4.W))
+        val storeVal = util.Decoupled(UInt(dWidth.W))
+      }
+    }
   });
 
   class InstrIODepend(val iWidth : Int) extends Bundle {
@@ -79,9 +40,8 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
 
   val instrs = initInstrs
 
-  val STATE_INIT = 0
-  val STATE_POP = 1
-  val state = RegInit(STATE_INIT.U(1.W))
+  val stateInit :: statePop :: Nil = util.Enum(2)
+  val state = RegInit(stateInit)
 
   val instrQueueIn = Wire(Flipped(util.Decoupled(new InstrIODepend(iWidth))))
   instrQueueIn.bits.ioDepend := false.B
@@ -105,10 +65,6 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
 
   val curInstrDepend = Reg(util.Valid(new InstrIODepend(iWidth)))
 
-  val curAdderInstr = Wire(new AdderInstruction)
-  curAdderInstr.code := curInstrDepend.bits.instr(2,0)
-  curAdderInstr.reg := curInstrDepend.bits.instr(3)
-
   val storeValReg = RegInit(false.B)
   when (storeValReg) {
     storeValReg := false.B
@@ -117,7 +73,7 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
 
   val memReqReg = RegInit(0.U(4.W))
   io.data.out.memReq.bits := memReqReg
-  io.data.out.memReq.valid := (memReqReg != 0.U) 
+  io.data.out.memReq.valid := (memReqReg =/= 0.U) 
 
   val dataInQueue = util.Queue(io.data.in, 3)
   val dataReadyReg = RegInit(false.B)
@@ -128,9 +84,7 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
     popReg := false.B
   }
 
-  instrQueue.ready := (state === STATE_POP.U)
-
-  //io.data.out.valid := io.data.out.memReq.valid | io.data.out.storeVal.valid
+  instrQueue.ready := (state === statePop)
 
   val memStateInit :: memStateWait :: Nil = util.Enum(2)
   val memStateReg = RegInit(memStateInit)
@@ -143,7 +97,7 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
     }
   }
 
-  when (state === STATE_INIT.U){
+  when (state === stateInit){
     when (curInstrDepend.valid) {
       when (curInstrDepend.bits.ioDepend) {
 
@@ -169,7 +123,7 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
 
             memReqReg := 0.U
 
-            state := STATE_POP.U
+            state := statePop
             popReg := true.B
             pcReg.bits := pcReg.bits + 1.U
             pcReg.valid := true.B
@@ -189,7 +143,7 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
                 }
               }
 
-              state := STATE_POP.U
+              state := statePop
               popReg := true.B
               pcReg.bits := pcReg.bits + 1.U
               pcReg.valid := true.B
@@ -205,43 +159,52 @@ abstract class ProcessingModule(dWidth : Int, iWidth : Int, queueDepth : Int) ex
           }
 
           pcReg.valid := true.B
-          state := STATE_POP.U
+          state := statePop
           popReg := true.B
         }
       }
     } .otherwise {
       pcReg.valid := false.B
-      state := STATE_POP.U
+      state := statePop
       popReg := true.B
     }
-  } .elsewhen (state === STATE_POP.U) {
+  } .elsewhen (state === statePop) {
     when (instrQueue.valid) {
-      state := STATE_INIT.U
+      state := stateInit
       curInstrDepend.bits := instrQueue.bits
       curInstrDepend.valid := true.B
       pcReg.valid := false.B
     } .otherwise {
       curInstrDepend.bits.ioDepend := false.B
-      val nopInstr = Wire(new AdderInstruction)
-      nopInstr.code := AdderInstruction.codeNOP
-      nopInstr.reg := 0.U(1.W)
-      curInstrDepend.bits.instr := nopInstr.asUInt
       curInstrDepend.valid := false.B
       pcReg.valid := !io.instr.in.valid
     }
   }
 }
 
-abstract class Instructions {
+object AdderInstruction {
 
-  def logic : Seq[InstructionLogic]
+  val codeNOP :: codeIncr1 :: codeIncrData :: codeStore :: codeBGT :: Nil = util.Enum(5)
+
+  val codeWidth = codeNOP.getWidth
+
+  val regWidth = 1
+
+  val width = codeWidth + regWidth
+
+  def createInt(codeVal : UInt, regVal : UInt) : BigInt = (new AdderInstruction).createInt(codeVal, regVal)
 }
 
-abstract class InstructionLogic (val name : String, val dataInDepend : Boolean, val dataOutDepend : Boolean) {
+class AdderInstruction(val code : UInt = UInt(AdderInstruction.codeWidth.W), val reg : UInt = UInt(AdderInstruction.regWidth.W)) extends Bundle {
+  require(code.getWidth == AdderInstruction.codeWidth)
+  require(reg.getWidth == AdderInstruction.regWidth)
 
-  def decode( instr : UInt) : Bool
-
-  def execute( instr : UInt) : Unit
+  def createInt(codeVal : UInt, regVal : UInt) : BigInt = {
+    def catValWidths(valWidthA : Tuple2[BigInt, Int], valWidthB : Tuple2[BigInt, Int]) : Tuple2[BigInt, Int] = {
+      (((valWidthB._1 << valWidthA._2) | valWidthA._1), (valWidthA._2 + valWidthB._2))
+    }
+    ((((codeVal :: regVal :: Nil) map (_.litValue)) zip (getElements map (_.getWidth))) reduce catValWidths)._1
+  }
 }
 
 class AdderModule(dWidth : Int, iWidth : Int, queueDepth : Int) extends ProcessingModule(dWidth, iWidth, queueDepth) {

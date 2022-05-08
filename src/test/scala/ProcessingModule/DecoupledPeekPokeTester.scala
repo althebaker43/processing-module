c8c72377eb3b1f55ab1dd657ef0edaf7dea2158e
Allen Baker <althebaker43@gmail.com>
2022-05-08 08:48:59 -0700

    Added increment-by-1 peek-poke test
    Moved expect statements in DecoupledPeekPokeTester after time
    step. Simplified code in ProcMod tester.

--- src/test/scala/ProcessingModule/DecoupledPeekPokeTester.scala
+++ src/test/scala/ProcessingModule/DecoupledPeekPokeTester.scala
@@ -23,40 +23,31 @@ abstract class DecoupledPeekPokeTester[T <: ProcessingModule](dut : T) extends P
     poke(dut.io.data.in.valid, 0)
     poke(dut.io.data.out.value.ready, 1)
 
+    // Poke inputs
     for (event <- cycle) {
 
       event match {
 
         case instrReq : InstrReq => {
           println("tester: Instruction request for address " + instrReq.addr)
-          expect(dut.io.instr.pc.bits, instrReq.addr)
-          expect(dut.io.instr.pc.valid, 1)
         }
 
         case instrRcv : InstrRcv => {
           println("tester: Receiving instruction " + instrRcv.instr)
-          expect(dut.io.instr.in.ready, 1)
           poke(dut.io.instr.in.valid, 1)
           poke(dut.io.instr.in.bits, instrRcv.instr)
         }
 
         case storeReq : StoreReq => {
           println("tester: Store request for address " + storeReq.addr + " with data " + storeReq.data)
-          expect(dut.io.data.out.addr.valid, 1)
-          expect(dut.io.data.out.value.valid, 1)
-          expect(dut.io.data.out.addr.bits, storeReq.addr)
-          expect(dut.io.data.out.value.bits, storeReq.data)
         }
 
         case loadReq : LoadReq => {
           println("tester: Load request for address " + loadReq.addr)
-          expect(dut.io.data.out.addr.valid, 1)
-          expect(dut.io.data.out.addr.bits, loadReq.addr)
         }
 
         case loadRcv : LoadRcv => {
           println("tester: Receiving load data " + loadRcv.data)
-          expect(dut.io.data.in.ready, 1)
           poke(dut.io.data.in.valid, 1)
           poke(dut.io.data.in.bits, loadRcv.data)
         }
@@ -64,5 +55,37 @@ abstract class DecoupledPeekPokeTester[T <: ProcessingModule](dut : T) extends P
     }
 
     step(1)
+
+    // Expect outputs
+    for (event <- cycle) {
+
+      event match {
+
+        case instrReq : InstrReq => {
+          expect(dut.io.instr.pc.bits, instrReq.addr)
+          expect(dut.io.instr.pc.valid, 1)
+        }
+
+        case instrRcv : InstrRcv => {
+          expect(dut.io.instr.in.ready, 1)
+        }
+
+        case storeReq : StoreReq => {
+          expect(dut.io.data.out.addr.valid, 1)
+          expect(dut.io.data.out.value.valid, 1)
+          expect(dut.io.data.out.addr.bits, storeReq.addr)
+          expect(dut.io.data.out.value.bits, storeReq.data)
+        }
+
+        case loadReq : LoadReq => {
+          expect(dut.io.data.out.addr.valid, 1)
+          expect(dut.io.data.out.addr.bits, loadReq.addr)
+        }
+
+        case loadRcv : LoadRcv => {
+          expect(dut.io.data.in.ready, 1)
+        }
+      }
+    }
   }
 }
--- src/test/scala/ProcessingModule/ProcessingModulePeekPokeTester.scala
+++ src/test/scala/ProcessingModule/ProcessingModulePeekPokeTester.scala
@@ -10,15 +10,45 @@ class ProcessingModulePeekPokeTester extends ChiselFlatSpec {
   val iWidth = AdderInstruction.width
   val queueDepth = 5
 
+  def nop : BigInt = AdderInstruction.createInt(AdderInstruction.codeNOP, 0.U, 0.U)
+  def incr1(regVal : Int) : BigInt = AdderInstruction.createInt(AdderInstruction.codeIncr1, regVal.U, 0.U)
+  def incrData(regVal : Int, addrVal : Int) : BigInt = AdderInstruction.createInt(AdderInstruction.codeIncrData, regVal.U, addrVal.U)
+  def store(regVal : Int, addrVal : Int) : BigInt = AdderInstruction.createInt(AdderInstruction.codeStore, regVal.U, addrVal.U)
+  def bgt(regVal : Int) : BigInt = AdderInstruction.createInt(AdderInstruction.codeBGT, regVal.U, 0.U)
+
+  def executeTest(testName : String)(testerGen : AdderModule => DecoupledPeekPokeTester[AdderModule]) : Boolean = Driver.execute(
+    Array("--generate-vcd-output", "on", "--target-dir", "test_run_dir/adder_" + testName), () => new AdderModule(4))(testerGen)
+
   behavior of "AdderModule"
 
-  ignore should "initialize correctly" in {
-    Driver.execute(Array("--generate-vcd-output", "on", "--target-dir", "test_run_dir/adder_init"), () => new AdderModule(4)) {
-      dut => new DecoupledPeekPokeTester(dut){
+  it should "initialize correctly" in {
+    executeTest("init"){
+      dut : AdderModule => new DecoupledPeekPokeTester(dut){
         def cycles = List(
-          List(new InstrReq(addr = 0)),
-          List(new InstrRcv(instr = AdderInstruction.createInt(AdderInstruction.codeStore, regVal=0.U, addrVal=3.U))),
-          List(new StoreReq(addr = 3, data = 0)))
+          List(new InstrReq(addr = 0)), // fetch 0
+          List(new InstrRcv(instr = store(0, 3))), // receive store
+          List(new InstrReq(addr = 1)), // fetch 1, decode store
+          List(new InstrRcv(instr = nop)), // receive nop, execute store
+          List(new StoreReq(addr = 3, data = 0), new InstrReq(addr = 2))) // store to 3, fetch 2
+      }
+    }
+  }
+
+  // TODO: resolve RAW hazard
+  it should "increment by 1" in {
+    executeTest("incr1"){
+      dut : AdderModule => new DecoupledPeekPokeTester(dut){
+        def cycles = List(
+          List(new InstrReq(addr = 0)), // fetch 0
+          List(new InstrRcv(instr = incr1(0))), // receive incr1
+          List(new InstrReq(addr = 1)), // decode incr1, fetch 1
+          Nil, // execute incr1
+          Nil, // memory incr1
+          List(new InstrRcv(instr = store(0, 6))), // receive store, writeback incr1
+          List(new InstrReq(addr = 2)), // decode store, fetch 2
+          List(new InstrRcv(instr = nop)), // execute store, receive nop
+          List(new StoreReq(addr = 6, data = 1), new InstrReq(addr = 3)) // store to 6, fetch 3
+        )
       }
     }
   }

5e6a231c27f91147252b84a72c2fdee37bd787ab
Allen Baker <althebaker43@gmail.com>
2022-12-03 07:36:00 -0800

    Initialized instruction memory for RISCV unit tests

--- src/test/scala/ProcessingModule/RISCV/RISCVLoaderModule.scala
+++ src/test/scala/ProcessingModule/RISCV/RISCVLoaderModule.scala
@@ -4,6 +4,7 @@ import chisel3._
 import scala.io.Source
 
 object RISCVLoaderModule {
+
   def getHexNum(str : String) : Long = {
     val nums = for ((char, index) <- str.toUpperCase.reverse.zipWithIndex) yield {
       val num : Long = char match {
@@ -28,6 +29,32 @@ object RISCVLoaderModule {
     }
     nums.reduce(_ | _)
   }
+
+  def getInstrArray(dumpPath : String) : Array[Long] = {
+
+    val instrs = for {
+      curLine <- Source.fromFile(dumpPath).getLines().toArray
+      tokens = curLine.split("\t")
+      if (tokens.length > 2)
+      if (tokens(0).startsWith("8"))
+      if (tokens(0).endsWith(":"))
+        } yield {
+      (getHexNum(tokens(0).stripPrefix("8").stripSuffix(":")),
+        getHexNum(tokens(1).strip()))
+    }
+
+    println("Num instrs: " + instrs.length)
+    for (pcInstr <- instrs) {
+      println("Init: PC: " + pcInstr._1.toHexString + ", Instr: " + pcInstr._2.toHexString)
+    }
+
+    val vecIdxs = for (instr <- instrs) yield instr._1 >> 2
+    val instrArr = new Array[Long]((vecIdxs.max + 1).toInt)
+    for (pcInstr <- instrs) {
+      instrArr((pcInstr._1 >> 2).toInt) = pcInstr._2
+    }
+    instrArr
+  }
 }
 
 class RISCVLoaderModule(dumpPath : String) extends Module {
@@ -39,20 +66,32 @@ class RISCVLoaderModule(dumpPath : String) extends Module {
   io.status.valid := false.B
   io.status.bits := false.B
 
-  val instrs = for {
-    curLine <- Source.fromFile(dumpPath).getLines().toArray
-    tokens = curLine.split("\t")
-    if (tokens.length > 2)
-    if (tokens(0).startsWith("8"))
-    if (tokens(0).endsWith(":"))
-      } yield {
-    (RISCVLoaderModule.getHexNum(tokens(0).stripPrefix("8").stripSuffix(":")),
-      RISCVLoaderModule.getHexNum(tokens(1).strip()))
+  val instrMem = VecInit(RISCVLoaderModule.getInstrArray(dumpPath).map(_.U(32.W)))
+
+  val instr = Wire(util.Decoupled(UInt(32.W)))
+  val pc = Wire(util.Valid(UInt(32.W)))
+
+  val instrReg = Reg(util.Valid(UInt(32.W)))
+  instr.valid := instrReg.valid
+  instr.bits := instrReg.bits
+  when (pc.valid & instr.ready) {
+    instrReg.valid := true.B
+    instrReg.bits := instrMem(pc.bits >> 2)
+  } .otherwise {
+    instrReg.valid := false.B
   }
 
-  println("Num instrs: " + instrs.length)
-
-  for (pcInstr <- instrs) {
-    println("PC: " + pcInstr._1.toHexString + ", Instr: " + pcInstr._2.toHexString)
+  pc.valid := false.B
+  instr.ready := false.B
+  val dummyPC = RegInit(0.U(32.W))
+  val prevDummyPC = RegNext(dummyPC)
+  pc.bits := dummyPC
+  when (dummyPC < 0x18c.U) {
+    pc.valid := true.B
+    instr.ready := true.B
+  }
+  when (instr.valid) {
+    printf("Sim: PC: %x, Instr: %x\n", prevDummyPC, instr.bits)
+    dummyPC := dummyPC + 4.U
   }
 }

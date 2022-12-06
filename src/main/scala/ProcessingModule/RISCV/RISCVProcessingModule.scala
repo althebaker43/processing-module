a76cd07e68c2f2f1192708ed68cc1df2e3f2d24a
Allen Baker <althebaker43@gmail.com>
2022-12-05 20:00:49 -0800

    Implemented ADDI instruction for RISCV

--- src/main/scala/ProcessingModule/RISCV/RISCVProcessingModule.scala
+++ src/main/scala/ProcessingModule/RISCV/RISCVProcessingModule.scala
@@ -1,12 +1,34 @@
 package ProcessingModule.RISCV
 
 import chisel3._
+import ProcessingModule.InstructionLogic
+
+class ADDI extends InstructionLogic("addi") {
+
+  def getOpcode(instr : UInt) : UInt = instr(6,2)
+  def isIType(instr : UInt) : Bool = getOpcode(instr) === "b00_100".U
+
+  def getFunc(instr : UInt) : UInt = instr(14,12)
+
+  def getIImm(instr : UInt) : UInt = instr(31,20)
+  def getIDest(instr : UInt) : UInt = instr(11,7)
+  def getISrc(instr : UInt) : UInt = instr(19,15)
+
+  override val numOps : Int = 1
+  override def decode(instr : UInt) : Bool = isIType(instr) & (getFunc(instr) === "b000".U)
+  override def getRFIndex(instr : UInt, opIndex : Int) : UInt = getISrc(instr)
+  override def writeRF() : Bool = true.B
+  override def getWriteIndex(instr : UInt, ops : Vec[UInt]) : UInt = getIDest(instr)
+  override def getData(instr : UInt, pc : UInt, ops : Vec[UInt]) : UInt = ops(0) + getIImm(instr)
+}
 
 class RISCVInstructions extends ProcessingModule.Instructions {
 
   override def logic = new ProcessingModule.InstructionLogic("dummy"){
     override def decode(instr : UInt) : Bool = false.B
-  } :: Nil
+  } ::
+  new ADDI ::
+  Nil
 }
 
 class RISCVProcessingModule (rfDepth : Int)
